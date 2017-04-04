e3174007434fdab040958c034b2947f1adb99bf9
André Yamamoto <aayandre94@gmail.com>
2017-04-04 11:42:47 -0300

    ex3 feito, mas tem varios IFS no meio

--- ListaSwitchCase/src/listaswitchcase/Ex3.java
+++ ListaSwitchCase/src/listaswitchcase/Ex3.java
@@ -18,74 +18,120 @@ public class Ex3 {
         Scanner ler = new Scanner(System.in);
 
         double valorUser;
-        int valorInteiro, n100 = 0, n50 = 0, n10 = 0, n5 = 0, n1 = 0;
+        int valorInteiro, n100 = 0, n50 = 0, n10 = 0, n5 = 0, n1 = 0, proces = 0,
+                resto100 = 0, resto50 = 0, resto10 = 0, resto5 = 0, resto1 = 0;
 
         System.out.println("----Dinheiro----");
 
         System.out.print("Digite um valor: ");
         valorUser = ler.nextDouble();
         valorInteiro = (int) valorUser;//transforma double em int.
-        
-        //Checa se o valor é maior, menor ou igual as notas e DA uma variavel para ele.
-        if (valorInteiro >= 100) {
-            int maior100 = valorInteiro;
-        } else if (valorInteiro >= 50 && valorInteiro < 100) {
-            int maior50 = valorInteiro;
-        } else if (valorInteiro >= 10 && valorInteiro < 50) {
-            int maior10 = valorInteiro;
-        } else if (valorInteiro >= 5 && valorInteiro < 10) {
-            int maior5 = valorInteiro;
-        } else if (valorInteiro >= 1 && valorInteiro < 5) {
-            int maior1 = valorInteiro;
-        } 
 
-//        switch (valorInteiro) {
-//            case maior100:
-//                if (valorInteiro > 100 || valorInteiro == 100) {
-//                    valorInteiro = valorInteiro - 100;
-//                    n100 = n100 + 1;
-//                }
-//                while (valorInteiro > 100);
-//                System.out.printf("%d nota(s) de R$100", n100);
-//                break;
-//            case 50:
-//                do {
-//                    if (valorInteiro > 50 || valorInteiro == 50) {
-//                        valorInteiro = valorInteiro - 50;
-//                        n50 = n50+1;
-//                    }
-//                } while (valorInteiro > 50);
-//                System.out.printf("%d nota(s) de R$50",n50);
-//                break;
-//            case 10:
-//                do {
-//                    if (valorInteiro > 10 || valorInteiro == 10) {
-//                        valorInteiro = valorInteiro - 10;
-//                        n10 = n10+1;
-//                    }
-//                } while (valorInteiro > 10);
-//                System.out.printf("%d nota(s) de R$10",n10);
-//                break;
-//            case 5:
-//                do {
-//                    if (valorInteiro > 5 || valorInteiro == 5) {
-//                        valorInteiro = valorInteiro - 5;
-//                        n5 = n5+1;
-//                    }
-//                } while (valorInteiro > 5);
-//                System.out.printf("%d nota(s) de R$5",n5);
-//                break;
-//            case 1:
-//                do {
-//                    if (valorInteiro > 1 || valorInteiro == 1) {
-//                        valorInteiro = valorInteiro - 1;
-//                        n1 = n1+1;
-//                    }
-//                } while (valorInteiro > 1);
-//                System.out.printf("%d nota(s) de R$1",n1);
-//                break;
-//
-//        }
+        //Checa se o valor é maior, menor ou igual as notas e cria um processo para utlizar o switch
+        if (valorInteiro >= 100) {
+            proces = 100;
+        } else if (valorInteiro >= 50 && valorInteiro < 100) {
+            proces = 50;
+        } else if (valorInteiro >= 10 && valorInteiro < 50) {
+            proces = 10;
+        } else if (valorInteiro >= 5 && valorInteiro < 10) {
+            proces = 5;
+        } else if (valorInteiro >= 1 && valorInteiro < 5) {
+            proces = 1;
+        } else {
+            System.out.println("Inválido.");
+        }
+//Switch usando o processo obtido anteriormente
+        switch (proces) {
+            case 100:
+                resto100 = valorInteiro % 100;
+                n100 = valorInteiro / 100;
+                resto50 = resto100 % 50;
+                n50 = resto100 / 50;
+                resto10 = resto50 % 10;
+                n10 = resto50 / 10;
+                resto5 = resto10 % 5;
+                n5 = resto10 / 5;
+                resto1 = resto5 % 1;
+                n1 = resto5 / 1;
+                break;
+            case 50:
+                resto50 = valorInteiro % 50;
+                n50 = valorInteiro / 50;
+                resto10 = resto50 % 10;
+                n10 = resto50 / 10;
+                resto5 = resto10 % 5;
+                n5 = resto10 / 5;
+                resto1 = resto5 % 1;
+                n1 = resto5 / 1;
+                break;
+            case 10:
+                resto10 = valorInteiro % 10;
+                n10 = valorInteiro / 10;
+                resto5 = resto10 % 5;
+                n5 = resto10 / 5;
+                resto1 = resto5 % 1;
+                n1 = resto5 / 1;
+                break;
+            case 5:
+                resto5 = valorInteiro % 5;
+                n5 = valorInteiro / 5;
+                resto1 = resto5 % 1;
+                n1 = resto5 / 1;
+                break;
+            case 1:
+                resto1 = valorInteiro % 1;
+                n1 = valorInteiro / 1;
+                break;
+            default:
+                break;
+
+        }
+//Saída, mostrando apenas as notas que forem usadas, nenhuma vai sair como ZERO        
+        if (n100 != 0) {
+            System.out.printf("%d nota(s) de R$100\n", n100);
+            if (n50 != 0) {
+                System.out.printf("%d nota(s) de R$50\n", n50);
+            }
+            if (n10 != 0) {
+                System.out.printf("%d nota(s) de R$10\n", n10);
+            }
+            if (n5 != 0) {
+                System.out.printf("%d nota(s) de R$5\n", n5);
+            }
+            if (n1 != 0) {
+                System.out.printf("%d nota(s) de R$1\n", n1);
+            }
+        } else if (n50 != 0) {
+            System.out.printf("%d nota(s) de R$50\n", n50);
+            if (n10 != 0) {
+                System.out.printf("%d nota(s) de R$10\n", n10);
+            }
+            if (n5 != 0) {
+                System.out.printf("%d nota(s) de R$5\n", n5);
+            }
+            if (n1 != 0) {
+                System.out.printf("%d nota(s) de R$1\n", n1);
+            }
+        } else if (n10 != 0) {
+            System.out.printf("%d nota(s) de R$10\n", n10);
+            if (n5 != 0) {
+                System.out.printf("%d nota(s) de R$5\n", n5);
+            }
+            if (n1 != 0) {
+                System.out.printf("%d nota(s) de R$1\n", n1);
+            }
+        } else if (n5 != 0) {
+            System.out.printf("%d nota(s) de R$5\n", n5);
+            if (n1 != 0) {
+                System.out.printf("%d nota(s) de R$1\n", n1);
+            }
+        } else if (n1 != 0) {
+            System.out.printf("%d nota(s) de R$1\n", n1);
+        } else {
+            System.out.println("Inválido.\n");
+        }
+
     }
 
 }
