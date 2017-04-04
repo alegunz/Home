cc9766a5d6e2a75c4820bbd9be34f365f92961ca
André Yamamoto <aayandre94@gmail.com>
2017-04-03 21:04:15 -0300

    ex8 feito

---
+++ ListaSwitchCase/src/listaswitchcase/Ex8.java
@@ -0,0 +1,86 @@
+/*
+ * André de Amorim Yamamoto
+ * aay.andre@outlook.com
+ * Turma A
+ */
+package listaswitchcase;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author andré.yamamoto <aayandre94@gmail.com>
+ */
+public class Ex8 {
+
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+
+//Variáveis
+        double ano, multa, tarifa;
+        int mes;
+
+//Entradas
+        System.out.print("Mês em que pagará ou foi paga a multa: ");
+        mes = ler.nextInt();
+        System.out.print("Valor da anuidade: ");
+        tarifa = ler.nextDouble();
+        ano = tarifa;
+//Processos e saídas        
+        switch (mes) {
+            case 1:
+                System.out.printf("\nValor normal: R$%.2f\n", tarifa);
+                break;
+            case 2:
+                tarifa = tarifa * Math.pow((1 + 0.05), 1);
+                System.out.printf("A anuidade é de R$%.2f\nCom a multa é: R$%.2f\n", ano, tarifa);
+                break;
+            case 3:
+                tarifa = tarifa * Math.pow((1 + 0.05), 2);
+                System.out.printf("A anuidade é de R$%.2f\nCom a multa é: R$%.2f\n", ano, tarifa);
+                break;
+            case 4:
+                tarifa = tarifa * Math.pow((1 + 0.05), 3);
+                System.out.printf("A anuidade é de R$%.2f\nCom a multa é: R$%.2f\n", ano, tarifa);
+                break;
+            case 5:
+                tarifa = tarifa * Math.pow((1 + 0.05), 4);
+                System.out.printf("A anuidade é de R$%.2f\nCom a multa é: R$%.2f\n", ano, tarifa);
+                break;
+            case 6:
+                tarifa = tarifa * Math.pow((1 + 0.05), 5);
+                System.out.printf("A anuidade é de R$%.2f\nCom a multa é: R$%.2f\n", ano, tarifa);
+                break;
+            case 7:
+                tarifa = tarifa * Math.pow((1 + 0.05), 6);
+                System.out.printf("A anuidade é de R$%.2f\nCom a multa é: R$%.2f\n", ano, tarifa);
+                break;
+            case 8:
+                tarifa = tarifa * Math.pow((1 + 0.05), 7);
+                System.out.printf("A anuidade é de R$%.2f\nCom a multa é: R$%.2f\n", ano, tarifa);
+                break;
+            case 9:
+                tarifa = tarifa * Math.pow((1 + 0.05), 8);
+                System.out.printf("A anuidade é de R$%.2f\nCom a multa é: R$%.2f\n", ano, tarifa);
+                break;
+            case 10:
+                tarifa = tarifa * Math.pow((1 + 0.05), 9);
+                System.out.printf("A anuidade é de R$%.2f\nCom a multa é: R$%.2f\n", ano, tarifa);
+                break;
+            case 11:
+                tarifa = tarifa * Math.pow((1 + 0.05), 10);
+                System.out.printf("A anuidade é de R$%.2f\nCom a multa é: R$%.2f\n", ano, tarifa);
+                break;
+            case 12:
+                tarifa = tarifa * Math.pow((1 + 0.05), 11);
+                System.out.printf("A anuidade é de R$%.2f\nCom a multa é: R$%.2f\n", ano, tarifa);
+                break;
+            default:
+                System.out.println("Mês inválido.");
+                break;
+        }
+
+    }
+
+}
