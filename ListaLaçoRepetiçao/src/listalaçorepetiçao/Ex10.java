f7e845242579035ea59edd78447211ba0a63c96f
André Yamamoto <aayandre94@gmail.com>
2017-04-20 16:38:06 -0300

    Ex10 feito

--- ListaLaçoRepetiçao/src/listalaçorepetiçao/Ex10.java
+++ ListaLaçoRepetiçao/src/listalaçorepetiçao/Ex10.java
@@ -11,4 +11,16 @@ package listalaçorepetiçao;
  */
 public class Ex10 {
     
+    public static void main(String[] args) {
+        
+        //Entrada
+        int soma = 0;
+        //Processo
+        for (int i = 1; i <= 20; i++) {
+            soma = soma + (1/i);           
+        }
+        //Saída
+        System.out.println(soma);
+    }
+    
 }
