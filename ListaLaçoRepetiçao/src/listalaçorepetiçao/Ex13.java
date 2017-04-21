3a8eb09efc6a5180f46286d16421b13de4da5567
André Yamamoto <aayandre94@gmail.com>
2017-04-21 17:29:39 -0300

    ex13 feito

---
+++ ListaLaçoRepetiçao/src/listalaçorepetiçao/Ex13.java
@@ -0,0 +1,38 @@
+/*
+ * André de Amorim Yamamoto
+ * aay.andre@outlook.com
+ * Turma A
+ */
+package listalaçorepetiçao;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author andré.yamamoto <aayandre94@gmail.com>
+ */
+public class Ex13 {
+
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+
+        //Entrada
+        System.out.print("Digite um número: ");
+        int num = ler.nextInt();
+
+        //Processo
+        int numerador = 1;
+        int denominador = 1;
+        int resultado = 0;
+        for (int i = 0; i < num; i++) {
+            numerador *= (i + 1);
+            denominador *= (2 * (i + 1)) - 1;
+            resultado += (numerador / denominador);
+        }
+                
+        //Saída
+        System.out.println(resultado);
+    }
+
+}
